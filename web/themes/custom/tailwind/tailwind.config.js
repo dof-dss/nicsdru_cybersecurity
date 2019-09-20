df1f34a4746c85f97159ecd0387501151b1e0e35
Orla O'Neill <orla.o'neill@finance-ni.gov.uk>
2019-09-20 17:52:29 +0100

    Some of the homepage blocks created and the untidy theming so far included for ease

---
+++ web/themes/custom/tailwind/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  prefix: 'tw-',
+  separator: '-',
+  theme: {
+    extend: {},
+    screens: {
+      'xs': '440px',
+      'sm': '640px',
+      'md': '765px',
+      'lg': '960px',
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+    }
+  },
+  variants: {},
+  plugins: [],
+  corePlugins: {
+    container: false
+  }
+}
