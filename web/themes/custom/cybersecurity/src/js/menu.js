24f0a90b8e6b6c76c77dc4dcbdb94c99c1e228c6
Orla O'Neill <orla.o'neill@finance-ni.gov.uk>
2019-10-14 14:09:37 +0100

    Style basic page and update menu.js

--- web/themes/custom/cybersecurity/src/js/menu.js
+++ web/themes/custom/cybersecurity/src/js/menu.js
@@ -1,17 +1,17 @@
-// Add class open to menu-mob on click of burger nav icon
-$(function () {
+(function ($) {
+  // Add class open to menu-mob on click of burger nav icon
   $(".burger-nav").on("click", function() {
     if (!$(".menu-mob").hasClass("open")) {
       $(".menu-mob").addClass("open");
     }
   });
-});
 
-// Remove class open from menu-mob on click of menu close icon
-$(function () {
+  // Remove class open from menu-mob on click of menu close icon
   $(".menu-close").on("click", function() {
     if ($(".menu-mob").hasClass("open")) {
       $(".menu-mob").removeClass("open");
     }
   });
-});
+})(jQuery);
+
+
