8b20ebb78f559a6d72eb84884b8763981718745b
Orla O'Neill <orla.o'neill@finance-ni.gov.uk>
2019-10-18 15:59:49 +0100

    Trying to fix jQuery

--- web/themes/custom/cybersecurity/src/js/menu.js
+++ web/themes/custom/cybersecurity/src/js/menu.js
@@ -5,13 +5,13 @@
     $(this).siblings().children(".menu_main--sub").addClass("tw-invisible");
   });
 
-  // When click anywhere but the menu link or its children, set submenu to invisible
-  $(document).click(function(e) {
-    $('.menu_main--item')
-      .not($('.menu_main--item').has($(e.target)))
-      .children('.menu_main--sub')
-      .addClass('tw-invisible');
-  });
+  // // When click anywhere but the menu link or its children, set submenu to invisible
+  // $(document).click(function(e) {
+  //   $(".menu_main--item")
+  //     .not($(".menu_main--item, .menu_main--sub").has($(e.target)))
+  //     .children(".menu_main--sub")
+  //     .addClass("tw-invisible");
+  // });
 
   // Add class open to menu-mob on click of burger nav icon
   $(".burger-nav").on("click", function() {
