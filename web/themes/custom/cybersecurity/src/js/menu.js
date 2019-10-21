e216e0af68c2fc4d25ecbc71de838f8407afbf86
Orla O'Neill <orla.o'neill@finance-ni.gov.uk>
2019-10-21 12:37:17 +0100

    Main navigation works on mobile

--- web/themes/custom/cybersecurity/src/js/menu.js
+++ web/themes/custom/cybersecurity/src/js/menu.js
@@ -1,17 +1,19 @@
 (function ($) {
   // Show main navigation sub-menu on click of parent
-  $(".menu_main--item").click(function() {
-    $(this).find(".menu_main--sub").toggleClass("tw-invisible");
-    $(this).siblings().children(".menu_main--sub").addClass("tw-invisible");
+  $(".menu_main--item h2, .menu_main--item .next").click(function() {
+    $(this).siblings(".menu_main--sub").toggleClass("tw-invisible");
+
+    // Hide any open main navigation sibling sub-menu on click of parent
+    $(this).parent(".menu_main--item").siblings().children(".menu_main--sub").addClass("tw-invisible");
   });
 
-  // // When click anywhere but the menu link or its children, set submenu to invisible
-  // $(document).click(function(e) {
-  //   $(".menu_main--item")
-  //     .not($(".menu_main--item, .menu_main--sub").has($(e.target)))
-  //     .children(".menu_main--sub")
-  //     .addClass("tw-invisible");
-  // });
+  // When click anywhere but the menu link or its children, set submenu to invisible
+  $(document).click(function(e) {
+    $(".menu_main--item")
+      .not($(".menu_main--item").has($(e.target)))
+      .children(".menu_main--sub")
+      .addClass("tw-invisible");
+  });
 
   // Add class open to menu-mob on click of burger nav icon
   $(".burger-nav").on("click", function() {
@@ -26,6 +28,10 @@
       $(".menu-mob").removeClass("open");
     }
   });
+
+  $(".menu_main--sub--buttons .back").click(function () {
+    $(this).parents(".menu_main--sub").toggleClass("tw-invisible");
+  })
 })(jQuery);
 
 
