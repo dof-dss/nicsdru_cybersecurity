9a3fc360b7a00dcd3e6b30abc0595cf3c1304b07
Orla O'Neill <orla.o'neill@finance-ni.gov.uk>
2019-11-06 15:33:14 +0000

    Apply css purge to stylesheet

--- web/themes/custom/cybersecurity/webpack.mix.js
+++ web/themes/custom/cybersecurity/webpack.mix.js
@@ -1,9 +1,20 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
 mix.sass('src/scss/styles.scss', 'css')
   .options({
     processCssUrls: false,
     postCss: [tailwindcss('./cybersecurity.config.js')],
+  })
+  .purgeCss({
+    enabled: true,
+
+    globs: [
+      path.join(__dirname, 'templates/*/*.html.twig'),
+      path.join(__dirname, 'src/scss/*/*.scss'),
+    ],
+
+    extensions: ['html.twig', 'html', 'php', 'scss'],
   });
 
