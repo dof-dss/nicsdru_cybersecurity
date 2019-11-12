882bc72b4028fef87f7d10f5b8c50423160d79a0
Orla O'Neill <orla.o'neill@finance-ni.gov.uk>
2019-11-12 16:06:18 +0000

    Add Drupal.behaviours.menu wrapper to menu.js

--- web/themes/custom/cybersecurity/src/js/menu.js
+++ web/themes/custom/cybersecurity/src/js/menu.js
@@ -1,45 +1,53 @@
-(function ($) {
-  // Show main navigation sub-menu on click of parent
-  $(".menu_main--item-text, .menu_main--item .next").click(function() {
-    $(this).siblings(".menu_main--sub").toggleClass("tw-hidden md-tw-block");
+(function ($, Drupal) {
+  console.log("open");
+  Drupal.behaviors.menu = {
+    attach: function(context, settings) {
+      // Show main navigation sub-menu on click of parent
+      $(".menu_main--item-text, .menu_main--item .next", context).click(function () {
+        console.log('item-click');
 
-    // Hide any open main navigation sibling sub-menu on click of parent
-    $(this).parent(".menu_main--item").siblings().children(".menu_main--sub").addClass("tw-hidden").removeClass("md-tw-block");
-  });
+        $(this).siblings(".menu_main--sub").toggleClass("tw-hidden md-tw-block");
 
-  // When click anywhere but the menu link or its children, set submenu to invisible
-  $(document).click(function(e) {
-    $(".menu_main--item")
-      .not($(".menu_main--item").has($(e.target)))
-      .children(".menu_main--sub")
-      .addClass("tw-hidden")
-      .removeClass("md-tw-block");
-  });
+        // Hide any open main navigation sibling sub-menu on click of parent
+        $(this).parent(".menu_main--item").siblings().children(".menu_main--sub").addClass("tw-hidden").removeClass("md-tw-block");
+      });
 
-  // When you click the gradient under the main menu submenu, the submenu is set to invisible
-  $(".menu_main--grad").click(function () {
-    $(this).parent(".menu_main--sub").addClass("tw-hidden").removeClass("md-tw-block");
-  });
+      // When click anywhere but the menu link or its children, set submenu to invisible
+      $(document, context).click(function (e) {
+        $(".menu_main--item")
+          .not($(".menu_main--item").has($(e.target)))
+          .children(".menu_main--sub")
+          .addClass("tw-hidden")
+          .removeClass("md-tw-block");
+      });
 
-  // Add class open to menu-mob on click of burger nav icon
-  $(".burger-nav").on("click", function() {
-    if (!$(".menu-mob").hasClass("open")) {
-      $(".menu-mob").addClass("open");
+      // When you click the gradient under the main menu submenu, the submenu is set to invisible
+      $(".menu_main--grad", context).click(function () {
+        $(this).parent(".menu_main--sub").addClass("tw-hidden").removeClass("md-tw-block");
+      });
+
+      // Add class open to menu-mob on click of burger nav icon
+      $(".burger-nav", context).on("click", function () {
+        console.log("Burger nav opened");
+        if (!$(".menu-mob").hasClass("open")) {
+          $(".menu-mob").addClass("open");
+        }
+      });
+
+      // Remove class open from menu-mob on click of menu close icon
+      $(".menu-close", context).on("click", function () {
+        console.log("Burger nav closed");
+
+        if ($(".menu-mob").hasClass("open")) {
+          $(".menu-mob").removeClass("open");
+        }
+      });
+
+      $(".menu_main--sub--buttons .back", context).click(function () {
+        if ($(window).width() < 765) {
+          $(this).parents(".menu_main--sub").toggleClass("tw-hidden");
+        }
+      })
     }
-  });
-
-  // Remove class open from menu-mob on click of menu close icon
-  $(".menu-close").on("click", function() {
-    if ($(".menu-mob").hasClass("open")) {
-      $(".menu-mob").removeClass("open");
-    }
-  });
-
-  $(".menu_main--sub--buttons .back").click(function () {
-    if ($(window).width() < 765) {
-      $(this).parents(".menu_main--sub").toggleClass("tw-hidden");
-    }
-  })
+  }
 })(jQuery);
-
-
