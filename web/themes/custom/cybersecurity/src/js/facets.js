0e3503541dce863b2e3c156a2e95c061b0cd0224
Orla O'Neill <orla.o'neill@finance-ni.gov.uk>
2019-11-13 11:18:09 +0000

    Add .ready wrapper to js files so it now works for all users.

--- web/themes/custom/cybersecurity/src/js/facets.js
+++ web/themes/custom/cybersecurity/src/js/facets.js
@@ -1,26 +1,30 @@
 (function ($) {
-  $(".block-facet--links h3").click(function () {
-    $(this).siblings(".facets-widget-links").toggleClass("tw-hidden");
-    if ($(this).children('svg').attr('data-icon') == 'chevron-down') {
-      $(this).children('svg').attr('data-icon', 'chevron-up');
-    } else {
-      $(this).children('svg').attr('data-icon', 'chevron-down');
-    };
-  });
+  $(document).ready(function () {
+    // Toggle the visibility of the facet-widget-links div and change the icon on click of the heading.
+    $(".block-facet--links h3").click(function () {
+      $(this).siblings(".facets-widget-links").toggleClass("tw-hidden");
+      if ($(this).children('svg').attr('data-icon') == 'chevron-down') {
+        $(this).children('svg').attr('data-icon', 'chevron-up');
+      } else {
+        $(this).children('svg').attr('data-icon', 'chevron-down');
+      };
+    });
 
-  $(function () {
-    if ($(".facet-item").children("a").hasClass("is-active")) {
-      $(".facets-widget-links").removeClass("tw-hidden");
+    // If one of the facet items are active then facets-widget-links should remain open on page reload.
+    $(function () {
+      if ($(".facet-item").children("a").hasClass("is-active")) {
+        $(".facets-widget-links").removeClass("tw-hidden");
 
-      if ($(".block-facet--links h3").children('svg').attr('data-icon') == 'chevron-down') {
-        $(".block-facet--links h3").children('svg').attr('data-icon', 'chevron-up');
+        if ($(".block-facet--links h3").children('svg').attr('data-icon') == 'chevron-down') {
+          $(".block-facet--links h3").children('svg').attr('data-icon', 'chevron-up');
+        }
+      } else {
+        $(".facets-widget-links").addClass("tw-hidden");
+
+        if ($(".block-facet--links h3").children('svg').attr('data-icon') == 'chevron-up') {
+          $(".block-facet--links h3").children('svg').attr('data-icon', 'chevron-down');
+        }
       }
-    } else {
-      $(".facets-widget-links").addClass("tw-hidden");
-
-      if ($(".block-facet--links h3").children('svg').attr('data-icon') == 'chevron-up') {
-        $(".block-facet--links h3").children('svg').attr('data-icon', 'chevron-down');
-      }
-    }
+    });
   });
 })(jQuery);
